6b762a856a03608959ffa165d5f8b6649c4b77b6
abinesh-abi <abinesh1729@gmail.com>
2022-03-05 13:19:47 +0530

    component row post added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
+import './App.css'
+
+//Components
 import NavBar from './Component/NavBar/NavBar'
 import  Banner from './Component/Banner/Banner'
+import  RowPost from './Component/RowPost/RowPost'
 
 export default function App() {
   return (
     <div>
       <NavBar/>
-      
       <Banner />
+
+      <RowPost />
     </div>
   );
 }
