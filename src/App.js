f4e8490e77c279e3a28217e7a860e152f2be09f4
abines-abi <abinesh1729@gmail.com>
2022-03-05 12:06:38 +0530

    create banner js and css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import NavBar from './Component/NavBar/NavBar'
+import  Banner from './Component/Banner/Banner'
 
 export default function App() {
   return (
     <div>
       <NavBar/>
+      
+      <Banner />
     </div>
   );
 }
